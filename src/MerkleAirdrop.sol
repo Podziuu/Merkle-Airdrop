15e976d87332477346d6ec93f2385adf64d53d91
Bartosz Podemski <bartekpodemski@gmail.com>
2024-08-25 14:37:53 +0200

    Merkle Test

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.24;
 import {IERC20, SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract MerkleAirdop {
+contract MerkleAirdrop {
     using SafeERC20 for IERC20;
 
     error MerkleAirdrop__InvalidProof();
